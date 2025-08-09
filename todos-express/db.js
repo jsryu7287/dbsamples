e9f5516a41b5fa26b7db8e62a1dd4539bea44ee4
jsryu7287 <jsryu7287@gmail.com>
2025-08-10 00:47:14 +0900

    Update db.js
    user/password 수정

--- todos-express/db.js
+++ todos-express/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: "localhost",
-    user: "root",
-    password: "rootroot",
+    user: "XXXX",
+    password: "XXXXXX",
     database: "my7287db",
     waitForConnections: true,
     connectionLimit: 10,
